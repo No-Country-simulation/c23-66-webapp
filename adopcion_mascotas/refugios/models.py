dde090c06c53b8d1a99b9b5e30ee4c018d3012e4
arielrz-dev <ruizarmando.dev@gmail.com>
2025-01-28 11:21:59 -0300

    rename direcciones

--- adopcion_mascotas/refugios/models.py
+++ adopcion_mascotas/refugios/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from direcciones.models import Direcciones
+from direcciones.models import Direccion
 from usuarios.models import Usuario
 
 
 # Create your models here.
-class Refugios(models.Model):
+class Refugio(models.Model):
 	nombre = models.CharField(max_length=100)
-	direccion = models.OneToOneField(Direcciones, on_delete=models.CASCADE, related_name="refugios")
+	direccion = models.OneToOneField(Direccion, on_delete=models.CASCADE, related_name="refugios")
 	usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="refugios")
 	telefono = models.CharField(max_length=100)
 	email = models.EmailField(max_length=100)
--- adopcion_mascotas/usuarios/models.py
+++ adopcion_mascotas/usuarios/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from direcciones.models import Direcciones
+from direcciones.models import Direccion
 
 
 # Create your models here.
@@ -8,6 +8,6 @@ class Usuario(models.Model):
 	nombre=models.CharField(max_length=50)
 	email=models.EmailField(max_length=50)
 	password=models.CharField(max_length=128)
-	direccion=models.OneToOneField(Direcciones, on_delete=models.CASCADE, null=True, blank=True)
+	direccion=models.OneToOneField(Direccion, on_delete=models.CASCADE, null=True, blank=True)
 	rol=models.CharField(max_length=50)
 	fecha_registro=models.DateField(auto_now_add=True)
